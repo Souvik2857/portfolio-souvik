1eea7d998a02b8fc616ad2f413a0c12ea23365bc
Souvik Sarkar <sarkarsouvik2343@gmail.com>
2026-05-05 12:10:13 +0530

    Code

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ activeLink.forEach((link) => {
         link.classList.add('active-link');
     });
 });
+const activeLinkMenubar = document.querySelectorAll('.link-sidebar')
+activeLinkMenubar.forEach((link) => {
+    link.addEventListener('click', () => {
+        activeLinkMenubar.forEach((l) => {
+            l.classList.remove('active-link');
+        });
+        link.classList.add('active-link');
+    });
+});
 function submit(){
     event.preventDefault();
     const user = document.querySelector('.name-input');
@@ -35,4 +44,14 @@ document.querySelectorAll('form').forEach((info) => {
             submit();
         }
     });
+});
+const menu=document.querySelector('.menu-bar');
+menu.addEventListener('click',()=>{
+    menu.style.display='none';
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='flex';
+    document.querySelector('.close-menu').addEventListener('click',()=>{
+        sidebar.style.display='none';
+        menu.style.display='block';
+    })
 });
